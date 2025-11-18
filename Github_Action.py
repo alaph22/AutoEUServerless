7e6ae629ac387c977936e50ab950b4d98b5b14e6
alaph22 <eraierbing1314@gmail.com>
2025-11-18 17:35:15 +0800

    Update Github_Action.py

--- Github_Action.py
+++ Github_Action.py
@@ -454,7 +454,6 @@ def main_handler(event, context):
 
 if __name__ == "__main__":
     main_handler(None, None)
-```# filepath: c:\Users\bing\euserv_alive
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 """
